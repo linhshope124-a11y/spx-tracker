0c36391acfcc2b74587d66e67ec3a3427842df91
linhshope124-a11y <linhshope124@gmail.com>
2026-09-25 19:51:58 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ================================================
 // SERVICE WORKER - SPX Tracker PWA v1.8
 // ================================================
-const CACHE_NAME = 'spx-tracker-v3';
+const CACHE_NAME = 'spx-tracker-v6';
 
 const PRECACHE_URLS = [
   './',
